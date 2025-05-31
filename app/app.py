9834eb385ac799828b558f7f5a275b330e0c8725
Shrawanib06 <157495901+Shrawanib06@users.noreply.github.com>
2025-05-31 16:52:09 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 CORS(app)
 
 # Google Drive file links
-model_url = "https://drive.google.com/file/d/1yX7jbL70N8Bc28ZK5hrPNnVmnuuQW1SR/view?usp=sharing"
-scaler_url = "https://drive.google.com/file/d/1k1ky0HAw7C9xNzTTTbC1l-XVDsSts9hm/view?usp=sharing"  # Updated link for scaler.pkl
+model_url = "https://drive.google.com/file/d/18UqqPS2bx-NIYooMo690T5BpH8uTnr_c/view?usp=sharing"
+scaler_url = "https://drive.google.com/file/d/1Uhmw-As5Sidf60e8LPojv3UXPb5VeDND/view?usp=sharing"  # Updated link for scaler.pkl
 
 # Paths for downloaded files
 model_path = "best_random_forest_model.pkl"
